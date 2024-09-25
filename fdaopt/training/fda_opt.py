6a3c29ecfc5db30b6a9c8ca89d0402a0ae74a2fa
miketheologitis <miketh@outlook.com.gr>
2024-09-25 14:00:24 +0300

    typo

--- fdaopt/training/fda_opt.py
+++ fdaopt/training/fda_opt.py
@@ -173,7 +173,7 @@ def fda_opt(hyperparams):
     # 2. RUN THE FDA-Opt ALGORITHM USING THE ESTIMATED THETA THRESHOLD
 
     # The variance threshold estimated using the linear-weighted-sum of the round variances
-    theta = linear_estimation_of_theta(round_variances)
+    theta = estimation_of_theta(round_variances)
 
     for r in range(r + 1, hyperparams['total_rounds'] - theta_estimation_rounds + r + 1):
 
